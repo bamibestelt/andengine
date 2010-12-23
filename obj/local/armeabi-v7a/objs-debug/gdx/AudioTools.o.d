760045e9b63af8e9f0c8e710d13b30d3766c0ae7
Matt Thompson <mthompson@hexwave.com>
2010-12-22 22:37:53 -0500

    Removed libgdx object files

--- obj/local/armeabi-v7a/objs-debug/gdx/AudioTools.o.d
+++
@@ -1,8 +0,0 @@
-/Users/fuzzy/Documents/hexwave_android/AndEngine/obj/local/armeabi-v7a/objs-debug/gdx/AudioTools.o: \
- /Users/fuzzy/Documents/hexwave_android/AndEngine/jni/AudioTools.cpp \
- /Users/fuzzy/Documents/hexwave_android/AndEngine/jni/AudioTools.h \
- /opt/android-ndk-r5/platforms/android-4/arch-arm/usr/include/jni.h
-
-/Users/fuzzy/Documents/hexwave_android/AndEngine/jni/AudioTools.h:
-
-/opt/android-ndk-r5/platforms/android-4/arch-arm/usr/include/jni.h:
--- obj/local/armeabi/objs-debug/gdx/AudioTools.o.d
+++
@@ -1,8 +0,0 @@
-/Users/fuzzy/Documents/hexwave_android/AndEngine/obj/local/armeabi/objs-debug/gdx/AudioTools.o: \
- /Users/fuzzy/Documents/hexwave_android/AndEngine/jni/AudioTools.cpp \
- /Users/fuzzy/Documents/hexwave_android/AndEngine/jni/AudioTools.h \
- /opt/android-ndk-r5/platforms/android-4/arch-arm/usr/include/jni.h
-
-/Users/fuzzy/Documents/hexwave_android/AndEngine/jni/AudioTools.h:
-
-/opt/android-ndk-r5/platforms/android-4/arch-arm/usr/include/jni.h:
